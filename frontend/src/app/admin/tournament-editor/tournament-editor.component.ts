1b02a0cfd4f62ad0cd9f3636dd86773f1a8f20e8
whv2li8 <wim.holvoet@colruytgroup.com>
2019-01-03 23:15:01 +0100

    intro bulma

--- frontend/src/app/admin/tournament-editor/tournament-editor.component.ts
+++ frontend/src/app/admin/tournament-editor/tournament-editor.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tournament-editor',
   templateUrl: './tournament-editor.component.html',
-  styleUrls: ['./tournament-editor.component.css']
+  styleUrls: ['./tournament-editor.component.scss']
 })
 export class TournamentEditorComponent implements OnInit {
 
--- frontend/src/app/admin/tournament-list/tournament-list.component.ts
+++ frontend/src/app/admin/tournament-list/tournament-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tournament-list',
   templateUrl: './tournament-list.component.html',
-  styleUrls: ['./tournament-list.component.css']
+  styleUrls: ['./tournament-list.component.scss']
 })
 export class TournamentListComponent implements OnInit {
 
